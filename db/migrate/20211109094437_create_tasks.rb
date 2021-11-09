e596d8e6b52b556e5739112e86d4ddddf82b060b
saskia aardenburg <saskia.aardenburg@gmail.com>
2021-11-09 10:45:49 +0100

    task model

---
+++ db/migrate/20211109094437_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.text :description
+      t.datetime :deadline
+      t.boolean :completed
+
+      t.timestamps
+    end
+  end
+end
